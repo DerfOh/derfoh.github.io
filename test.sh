705fd4f337580598b59796b58dca8fe5e10f6134
Fredrick Paulin <fredrick.p@outlook.com>
2018-08-19 21:28:25 -0400

    Add jekyll command to test script

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+jekyll serve -B
+
 echo "Waiting 30 seconds for service to come up..."
 sleep 30
 
 echo "HTTP Status code returned: "
-curl -sL -w "%{http_code}\\n" "0.0.0.0:80" -o /dev/null 
+curl -sL -w "%{http_code}\\n" "0.0.0.0:4000" -o /dev/null 
