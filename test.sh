527abcfffbe5c11d11c5196d8d054bd2a9a68a84
Fredrick Paulin <fredrick.p@outlook.com>
2018-08-19 21:19:15 -0400

    Add wait to test script

--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-pwd
-ls -lrt
-ps -ef
-/sbin/my_init >> /dev/null
+echo "Waiting 30 seconds for service to come up..."
+sleep 30
+
 echo "HTTP Status code returned: "
 curl -sL -w "%{http_code}\\n" "0.0.0.0:80" -o /dev/null
