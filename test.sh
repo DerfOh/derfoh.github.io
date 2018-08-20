3855e185ff7296fe0007919edc5366d0f441960e
Fredrick Paulin <fredrick.p@outlook.com>
2018-08-19 20:16:30 -0400

    Debug test

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-echo "HTTP Status code returned: "
-curl -sL -w "%{http_code}\\n" "0.0.0.0:80" -o /dev/null
 pwd
 ls -lrt
+ps -ef
+echo "HTTP Status code returned: "
+curl -sL -w "%{http_code}\\n" "0.0.0.0:80" -o /dev/null
